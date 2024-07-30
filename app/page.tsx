8b5017364908c4fc942aa93d77e3d3a4bf5f8efc
shivam98nitt <shivamsingh24680@gmail.com>
2024-07-30 18:39:19 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/main/Footer";
 import { Hero } from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 import Image from "next/image";
 
@@ -8,6 +10,8 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <Projects/>
+        <Footer/>
       </div>
     </main>
 
