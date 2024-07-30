79b81467d69751cb3fd17f1a1e23ae10c08c755d
shivam98nitt <shivamsingh24680@gmail.com>
2024-07-30 18:57:10 +0530

    vercel testing

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -49,7 +49,7 @@ const HeroContent = () => {
                     variants={slideInFromLeft(1)}
                     className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
                 >
-                    Learn More!
+                    Download Resume
                 </motion.a>
             </div>
 
